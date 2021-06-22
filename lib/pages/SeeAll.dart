9d1d8bc87d9b3f7bac3074854d416dc0db9899ce
Shubham Deshmukh <dshubham1295@gmail.com>
2021-06-22 23:05:21 +0530

    Fixed Conflicts

--- lib/pages/SeeAll.dart
+++ lib/pages/SeeAll.dart
@@ -1,4 +1,3 @@
-import 'package:Grabzo/constant/constants.dart';
 import 'package:Grabzo/model/ItemsBean.dart';
 import 'package:Grabzo/service/Items.dart';
 import 'package:flutter/material.dart';
@@ -25,13 +24,13 @@ class _SeeAllState extends State<SeeAll> {
             if (snapshot.hasError)
               return Center(child: Text('Error: ${snapshot.error}'));
             else {
-              return scafold(context, snapshot.data);
+              return seeAllScaffold(context, snapshot.data);
             }
           }
         });
   }
 
-  Scaffold scafold(BuildContext context, ItemsBean data) {
+  Scaffold seeAllScaffold(BuildContext context, ItemsBean data) {
     return Scaffold(
       backgroundColor: Color(0xFFFAFAFA),
       appBar: AppBar(
@@ -60,90 +59,9 @@ class _SeeAllState extends State<SeeAll> {
             InkWell(
               onTap: () {
                 Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => ProductInfo(id: item.itemId),
-            },
-            child: Card(
-                color: Colors.white,
-                elevation: 0,
-                // shape: RoundedRectangleBorder(
-                //   borderRadius: const BorderRadius.all(
-                //     Radius.circular(5.0),
-                //   ),
-                // ),
-                child: Container(
-                  width: 170,
-                  height: 210,
-                  child: Column(
-                    children: <Widget>[
-                      Stack(
-                        children: <Widget>[
-                          Align(
-                            alignment: Alignment.centerLeft,
-                            child: Center(
-                                child: Image.asset(
-                              'assets/images/popular_foods/ic_popular_food_1.png',
-                              width: 130,
-                              height: 140,
-                            )),
-                          )
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Container(
-                            alignment: Alignment.bottomLeft,
-                            padding: EdgeInsets.only(left: 5, top: 5),
-                            child: Text('Food Name',
-                                style: TextStyle(
-                                    color: Color(0xFF6e6e71),
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.w500)),
-                          ),
-                          Container(
-                            alignment: Alignment.topRight,
-                            padding: EdgeInsets.only(right: 5),
-                            child: Container(
-                              height: 28,
-                              width: 28,
-                              decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: Colors.white70,
-                                  boxShadow: [
-                                    BoxShadow(
-                                      color: Color(0xFFfae3e2),
-                                      blurRadius: 25.0,
-                                      offset: Offset(0.0, 0.75),
-                                    ),
-                                  ]),
-                              child: Icon(
-                                Icons.near_me_outlined,
-                                color: Color(0xfffb3132),
-                                size: 16,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Container(
-                            alignment: Alignment.bottomLeft,
-                            padding: EdgeInsets.only(left: 5, top: 5, right: 5),
-                            child: Text('\$' + '17.30',
-                                style: TextStyle(
-                                    color: Color(0xFF6e6e71),
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.w600)),
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-                );
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => ProductInfo(id: item.itemId)));
               },
               child: Card(
                   color: Colors.white,
@@ -177,7 +95,7 @@ class _SeeAllState extends State<SeeAll> {
                             Container(
                               alignment: Alignment.bottomLeft,
                               padding: EdgeInsets.only(left: 5, top: 5),
-                              child: Text(item.itemName,
+                              child: Text('Food Name',
                                   style: TextStyle(
                                       color: Color(0xFF6e6e71),
                                       fontSize: 15,
@@ -200,7 +118,7 @@ class _SeeAllState extends State<SeeAll> {
                                       ),
                                     ]),
                                 child: Icon(
-                                  Icons.favorite,
+                                  Icons.near_me_outlined,
                                   color: Color(0xfffb3132),
                                   size: 16,
                                 ),
@@ -215,9 +133,7 @@ class _SeeAllState extends State<SeeAll> {
                               alignment: Alignment.bottomLeft,
                               padding:
                                   EdgeInsets.only(left: 5, top: 5, right: 5),
-                              child: Text(
-                                  Constants.rupeesSymbol +
-                                      item.itemPrice.toString(),
+                              child: Text('\$' + '17.30',
                                   style: TextStyle(
                                       color: Color(0xFF6e6e71),
                                       fontSize: 12,
@@ -229,6 +145,246 @@ class _SeeAllState extends State<SeeAll> {
                     ),
                   )),
             ),
+          Card(
+              color: Colors.white,
+              elevation: 0,
+              // shape: RoundedRectangleBorder(
+              //   borderRadius: const BorderRadius.all(
+              //     Radius.circular(5.0),
+              //   ),
+              // ),
+              child: Container(
+                width: 170,
+                height: 210,
+                child: Column(
+                  children: <Widget>[
+                    Stack(
+                      children: <Widget>[
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Center(
+                              child: Image.asset(
+                            'assets/images/popular_foods/ic_popular_food_1.png',
+                            width: 130,
+                            height: 140,
+                          )),
+                        )
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Container(
+                          alignment: Alignment.bottomLeft,
+                          padding: EdgeInsets.only(left: 5, top: 5),
+                          child: Text('Food Name',
+                              style: TextStyle(
+                                  color: Color(0xFF6e6e71),
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w500)),
+                        ),
+                        Container(
+                          alignment: Alignment.topRight,
+                          padding: EdgeInsets.only(right: 5),
+                          child: Container(
+                            height: 28,
+                            width: 28,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.white70,
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Color(0xFFfae3e2),
+                                    blurRadius: 25.0,
+                                    offset: Offset(0.0, 0.75),
+                                  ),
+                                ]),
+                            child: Icon(
+                              Icons.favorite,
+                              color: Color(0xfffb3132),
+                              size: 16,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Container(
+                          alignment: Alignment.bottomLeft,
+                          padding: EdgeInsets.only(left: 5, top: 5, right: 5),
+                          child: Text('\$' + '17.30',
+                              style: TextStyle(
+                                  color: Color(0xFF6e6e71),
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w600)),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              )),
+          Card(
+              color: Colors.white,
+              elevation: 0,
+              // shape: RoundedRectangleBorder(
+              //   borderRadius: const BorderRadius.all(
+              //     Radius.circular(5.0),
+              //   ),
+              // ),
+              child: Container(
+                width: 170,
+                height: 210,
+                child: Column(
+                  children: <Widget>[
+                    Stack(
+                      children: <Widget>[
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Center(
+                              child: Image.asset(
+                            'assets/images/popular_foods/ic_popular_food_1.png',
+                            width: 130,
+                            height: 140,
+                          )),
+                        )
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Container(
+                          alignment: Alignment.bottomLeft,
+                          padding: EdgeInsets.only(left: 5, top: 5),
+                          child: Text('Food Name',
+                              style: TextStyle(
+                                  color: Color(0xFF6e6e71),
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w500)),
+                        ),
+                        Container(
+                          alignment: Alignment.topRight,
+                          padding: EdgeInsets.only(right: 5),
+                          child: Container(
+                            height: 28,
+                            width: 28,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.white70,
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Color(0xFFfae3e2),
+                                    blurRadius: 25.0,
+                                    offset: Offset(0.0, 0.75),
+                                  ),
+                                ]),
+                            child: Icon(
+                              Icons.favorite,
+                              color: Color(0xfffb3132),
+                              size: 16,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Container(
+                          alignment: Alignment.bottomLeft,
+                          padding: EdgeInsets.only(left: 5, top: 5, right: 5),
+                          child: Text('\$' + '17.30',
+                              style: TextStyle(
+                                  color: Color(0xFF6e6e71),
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w600)),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              )),
+          Card(
+              color: Colors.white,
+              elevation: 0,
+              // shape: RoundedRectangleBorder(
+              //   borderRadius: const BorderRadius.all(
+              //     Radius.circular(5.0),
+              //   ),
+              // ),
+              child: Container(
+                width: 170,
+                height: 210,
+                child: Column(
+                  children: <Widget>[
+                    Stack(
+                      children: <Widget>[
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Center(
+                              child: Image.asset(
+                            'assets/images/popular_foods/ic_popular_food_1.png',
+                            width: 130,
+                            height: 140,
+                          )),
+                        )
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Container(
+                          alignment: Alignment.bottomLeft,
+                          padding: EdgeInsets.only(left: 5, top: 5),
+                          child: Text('Food Name',
+                              style: TextStyle(
+                                  color: Color(0xFF6e6e71),
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w500)),
+                        ),
+                        Container(
+                          alignment: Alignment.topRight,
+                          padding: EdgeInsets.only(right: 5),
+                          child: Container(
+                            height: 28,
+                            width: 28,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.white70,
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Color(0xFFfae3e2),
+                                    blurRadius: 25.0,
+                                    offset: Offset(0.0, 0.75),
+                                  ),
+                                ]),
+                            child: Icon(
+                              Icons.favorite,
+                              color: Color(0xfffb3132),
+                              size: 16,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Container(
+                          alignment: Alignment.bottomLeft,
+                          padding: EdgeInsets.only(left: 5, top: 5, right: 5),
+                          child: Text('\$' + '17.30',
+                              style: TextStyle(
+                                  color: Color(0xFF6e6e71),
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w600)),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              )),
         ],
       ),
     );
